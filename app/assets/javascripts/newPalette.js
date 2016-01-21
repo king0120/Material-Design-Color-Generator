e802dc6a79b106be2829f1160837980500f1b9b3
Jamie King <king0120@gmail.com>
2016-01-21 11:44:10 -0500

    Show Styling

--- app/assets/javascripts/newPalette.js
+++ app/assets/javascripts/newPalette.js
@@ -1,7 +1,7 @@
 var primaryColor, secondaryColor;
 var backgroundNumber = 1;
-var pBox, sBox;
-
+var pBox, sBox, hexValue;
+var pri1, pri2, pri3, acc;
 
 //Renders clickable boxes to the screen. Runs once to select primary colors and again to run secondary
 function renderBox(boxes, array, light) {
@@ -11,7 +11,7 @@ function renderBox(boxes, array, light) {
     "600", "700",
     "800", "900"
   ];
-  var hexValue, background;
+  var background;
 
   for (i = 0; i < array.length; i++) {
     //Sets variables for each stage of the app
@@ -23,7 +23,6 @@ function renderBox(boxes, array, light) {
         fontColor = array[i].light;
         break;
       case "secondary":
-        console.log("secondary");
         hexValue = array[i].accent_400;
         individualName[i] = array[i].name;
         background = array[i].accent_400;
@@ -57,7 +56,7 @@ function renderBox(boxes, array, light) {
       }
     } //End if statement checking for accents
   } //End for loop
-  console.log(backgroundNumber);
+  // console.log(backgroundNumber);
   clicks();
 } //End renderBox
 
@@ -91,8 +90,6 @@ function accentShade(){
 function backgroundManipulation() {
   //Changes the background color based on the element being hovered over
   $('.colorBox').hover(function() {
-    $(this).animate({'transform': 'scale(1.1)'})
-    console.log('hello');
     var backgroundColor = $(this).css("background-color");
     $('.background' + backgroundNumber).css('background', backgroundColor);
     // if($(this).hasClass('primary') || $(this).hasClass('primaryShades')){
@@ -110,6 +107,8 @@ function clicks() {
   $('.primary').click(function() {
     backgroundNumber++;
     primaryColor = $(this).find('p').html();
+    pri1 = $(this).find('.renderHex').html();
+    console.log(pri1);
     $('.primary').hide();
     pBox = colorsJson.filter(function(x) {
       if (x.name === primaryColor) {
@@ -124,6 +123,11 @@ function clicks() {
 
   $('.primaryShades').click(function() {
     shadesColor = $(this).find('p').html();
+    if (pri2===undefined){
+      pri2 = $(this).find('.renderHex').html();
+    } else {
+      pri3 = $(this).find('.renderHex').html();
+    }
     $('.primaryShades').hide();
     if (backgroundNumber < 3) {
       shades(primaryColor);
@@ -138,9 +142,8 @@ function clicks() {
   // when a secondary color is clicked, its
   // value is saved and the secondary colors are hidden.
   $('.secondary').click(function() {
-    console.log('clicked');
     secondaryColor = $(this).find('p').html();
-    console.log(secondaryColor);
+    acc = $(this).find('.renderHex').html();
     $('.secondary').hide();
     sBox = colorsJson.filter(function(x) {
       if (x.name === secondaryColor) {
@@ -156,7 +159,6 @@ function clicks() {
 
   $('.secondaryShades').click(function(){
     SecondaryColor = $(this).find('p').html();
-    console.log(secondaryColor);
     $('.secondaryShades').hide();
     $('#paletteName').removeClass('hidden');
     $('#paletteSubmit').removeClass('hidden');
@@ -166,16 +168,7 @@ function clicks() {
 }
 
 function preview() {
-  // for (i=1; i<=4; i++){
-  //   console.log('.background'+i);
-  //   $('.background'+i).animate({height: '25%', width: '12.5%', top: '50%', margin: '0'});
-  //   //.css('animation', 'palettePreview 1s ease-in forward');
-  // }
   $('.backgrounds').animate({height: '50vw', top: '50%'});
-  // $('.primarySelected').addClass('colorBox')
-  //   .css('background', pBox.palette_500).append("<p>" + pBox.name + "</p>");
-  // $('.secondarySelected').addClass('colorBox')
-  //   .css('background', sBox.accent_200).append("<p>" + sBox.name + "</p>");
 
   submit(pBox, sBox);
 }
@@ -187,9 +180,9 @@ function submit(primary, secondary) {
 
   $('#palette_primary_name').val(primary.name);
   $('#palette_secondary_name').val(secondary.name);
-  $('#palette_dark_primary').val($('.background2').css('background-color'));
-  $('#palette_light_primary').val($('.background3').css('background-color'));
-  $('#palette_primary').val($('.background1').css('background-color'));
-  $('#palette_secondary').val($('.background4').css('background-color'));
+  $('#palette_dark_primary').val(pri2);
+  $('#palette_light_primary').val(pri3);
+  $('#palette_primary').val(pri1);
+  $('#palette_secondary').val(acc);
   $('#palette_text_color').val(primary.light);
 }
